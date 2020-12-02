b397d78b3b0f3a012adfbb0d3c6b15a9ecca7375
Arindam Dutta <66781582+arindam1903@users.noreply.github.com>
2020-12-03 05:10:12 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
 
 // declare class
-
+let Shape = require("./shape");
+// declare class
+class Circle extends Shape {
+  constructor() {
+    super();
+    this.color = "blue";
+  }
+  calculateArea() {}
+}
+module.exports = Circle;
 
 // export class using module.exports
